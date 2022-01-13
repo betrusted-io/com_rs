350f00aeca461458dd30abb6fc680bdabaef353d
bunnie <bunnie@kosagi.com>
2022-01-13 16:23:54 +0800

    add rev reporting command

--- src/lib.rs
+++ src/lib.rs
@@ -60,6 +60,7 @@ impl ComState {
     pub const EC_GIT_REV: ComSpec            = ComSpec{verb: 0x4001, w_words: 0,     r_words: 3     ,response: false};
     pub const UPTIME: ComSpec                = ComSpec{verb: 0x4002, w_words: 0,     r_words: 4     ,response: false};
     pub const TRNG_SEED: ComSpec             = ComSpec{verb: 0x4003, w_words: 8,     r_words: 0     ,response: false};
+    pub const EC_SW_TAG: ComSpec             = ComSpec{verb: 0x4004, w_words: 0,     r_words: 16    ,response: false};
 
     // charger "dangerous" commands
     pub const CHG_START: ComSpec             = ComSpec{verb: 0x5A00, w_words: 0,     r_words: 0     ,response: false};
